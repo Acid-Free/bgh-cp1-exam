862c0c63f8c23ca42d6443ca55f5b821aee424ce
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2024-04-19 15:17:42 +0800

    Configure primevue - Define layers for proper css assignment - Load primevue to app

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,23 +1,37 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import "primevue/resources/themes/aura-light-blue/theme.css";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createApp, h, DefineComponent } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+import DataTable from "primevue/datatable";
+import Button from "primevue/button";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue)
+            .use(ToastService)
+            .component("DataTable", DataTable)
+            .component("Button", Button)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
