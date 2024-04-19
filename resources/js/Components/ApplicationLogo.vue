c1b6701397e04917e4ad21cc76a08aa0d889f602
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2024-04-19 15:28:57 +0800

    Format to apply layout

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string }>();
+const props = defineProps<{ class?: string }>()
 </script>
 
 <template>
-    <div :class="props.class">
-        <img
-            :class="props.class"
-            src="/bghmc-square-200.png"
-            alt="BGHMC round logo"
-        />
-    </div>
+  <div :class="props.class">
+    <img :class="props.class" src="/bghmc-square-200.png" alt="BGHMC round logo" />
+  </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,65 +1,59 @@
 <script setup lang="ts">
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
-import SecondaryButton from "@/Components/SecondaryButton.vue";
-import { Head, Link, router } from "@inertiajs/vue3";
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import { Head, Link, router } from '@inertiajs/vue3'
 
 defineProps<{
-    canLogin?: boolean;
-    canRegister?: boolean;
-    laravelVersion: string;
-    phpVersion: string;
-}>();
+  canLogin?: boolean
+  canRegister?: boolean
+  laravelVersion: string
+  phpVersion: string
+}>()
 </script>
 
 <template>
-    <Head title="Welcome" />
-    <div
-        class="bg-gray-50 text-black/70 dark:bg-black dark:text-white/50 h-[100vh] flex flex-col justify-between"
-    >
-        <header class="grid grid-cols-2 items-center gap-2 py-8 lg:grid-cols-3">
-            <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                <template v-if="!$page.props.auth.user">
-                    <Link
-                        :href="route('login')"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                    >
-                        Log in
-                    </Link>
+  <Head title="Welcome" />
+  <div
+    class="flex h-[100vh] flex-col justify-between bg-gray-50 text-black/70 dark:bg-black dark:text-white/50"
+  >
+    <header class="grid grid-cols-2 items-center gap-2 py-8 lg:grid-cols-3">
+      <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+        <template v-if="!$page.props.auth.user">
+          <Link
+            :href="route('login')"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+          >
+            Log in
+          </Link>
 
-                    <Link
-                        v-if="canRegister"
-                        :href="route('register')"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                    >
-                        Register
-                    </Link>
-                </template>
-            </nav>
-        </header>
+          <Link
+            v-if="canRegister"
+            :href="route('register')"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+          >
+            Register
+          </Link>
+        </template>
+      </nav>
+    </header>
 
-        <main class="mt-6 flex-1 flex flex-col">
-            <div
-                class="flex flex-col sm:flex-row items-center gap-4 sm:gap-12 justify-center"
-            >
-                <ApplicationLogo class="w-24" />
-                <h1 class="text-4xl sm:text-[5rem] font-bold">
-                    BGHMC CP1 Exam
-                </h1>
-            </div>
-            <div
-                class="flex flex-1 [&>*]:flex-1 gap-4 sm:gap-12 p-12 sm:p-52 flex-col sm:flex-row"
-            >
-                <SecondaryButton @click="router.visit(route('patients'))">
-                    <span class="text-2xl sm:text-[3rem]">Patients</span>
-                </SecondaryButton>
-                <SecondaryButton @click="router.visit(route('admissions'))">
-                    <span class="text-2xl sm:text-[3rem]">Admissions</span>
-                </SecondaryButton>
-            </div>
-        </main>
+    <main class="mt-6 flex flex-1 flex-col">
+      <div class="flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-12">
+        <ApplicationLogo class="w-24" />
+        <h1 class="text-4xl font-bold sm:text-[5rem]">BGHMC CP1 Exam</h1>
+      </div>
+      <div class="flex flex-1 flex-col gap-4 p-12 sm:flex-row sm:gap-12 sm:p-52 [&>*]:flex-1">
+        <SecondaryButton @click="router.visit(route('patients'))">
+          <span class="text-2xl sm:text-[3rem]">Patients</span>
+        </SecondaryButton>
+        <SecondaryButton @click="router.visit(route('admissions'))">
+          <span class="text-2xl sm:text-[3rem]">Admissions</span>
+        </SecondaryButton>
+      </div>
+    </main>
 
-        <footer class="py-8 text-center text-sm text-black dark:text-white/70">
-            Sian Caleb Domasig 2024
-        </footer>
-    </div>
+    <footer class="py-8 text-center text-sm text-black dark:text-white/70">
+      Sian Caleb Domasig 2024
+    </footer>
+  </div>
 </template>
